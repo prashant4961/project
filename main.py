e5178eca67cd63c78dbe55d64c00c6c831e53b2d
prashant4961 <prw4961@gmail.com>
2022-05-19 17:06:29 +0530

    modified

--- main.py
+++ main.py
@@ -89,4 +89,4 @@ def subscribed():
     return render_template('index.html',prediction= final_result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=8080)
